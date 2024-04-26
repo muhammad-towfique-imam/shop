bfcf143f540bb6b990e304ba90f01e3ce7758444
Md. Towfique Imam <mithu1408@gmail.com>
2024-04-27 02:05:14 +0600

    Initial project generated from start.spring.io

---
+++ src/test/java/com/cyanice/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyanice.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
