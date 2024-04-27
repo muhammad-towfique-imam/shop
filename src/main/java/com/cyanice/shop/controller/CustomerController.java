800a6bfa385c5c2a24bf3e71d1826b906c5e4d3c
Md. Towfique Imam <mithu1408@gmail.com>
2024-04-27 14:45:26 +0600

    Modified customer controller class to return real data

--- src/main/java/com/cyanice/shop/controller/CustomerController.java
+++ src/main/java/com/cyanice/shop/controller/CustomerController.java
@@ -2,23 +2,27 @@ package com.cyanice.shop.controller;
 
 
 import com.cyanice.shop.dto.ProductDto;
+import com.cyanice.shop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
+    private ProductService productService;
+
+    @Autowired
+    public CustomerController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/{id}/wishlist")
     public List<ProductDto> getUserWishlist(@PathVariable long id) {
-        return Arrays.asList(
-                ProductDto.builder().name("iPhone").price(1000.0).build(),
-                ProductDto.builder().name("iPod").price(300.0).build(),
-                ProductDto.builder().name("iMac").price(3000.0).build()
-        );
+        return productService.getCustomerWishlist(id);
     }
 }
--- src/test/java/com/cyanice/shop/controller/CustomerControllerTests.java
+++ src/test/java/com/cyanice/shop/controller/CustomerControllerTests.java
@@ -1,15 +1,21 @@
 package com.cyanice.shop.controller;
 
+import com.cyanice.shop.dto.ProductDto;
+import com.cyanice.shop.service.ProductService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(CustomerController.class)
@@ -18,8 +24,17 @@ public class CustomerControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ProductService productService;
+
     @Test
     public void shouldGetCustomerWishlist() throws Exception {
+        when(productService.getCustomerWishlist(1)).thenReturn(Arrays.asList(
+                ProductDto.builder().name("iPhone").price(1000.0).build(),
+                ProductDto.builder().name("iPod").price(300.0).build(),
+                ProductDto.builder().name("iMac").price(3000.0).build()
+        ));
+
         ResultActions response = mockMvc.perform(get("/api/customer/1/wishlist")
                 .contentType(MediaType.APPLICATION_JSON));
 
