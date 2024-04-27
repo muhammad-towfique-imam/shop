c1252ff5e1b9acfc67dd6bf280ccb347b7618cc6
Md. Towfique Imam <mithu1408@gmail.com>
2024-04-27 13:54:50 +0600

    Added test case for customer wishlist

---
+++ src/test/java/com/cyanice/shop/controller/CustomerControllerTests.java
@@ -0,0 +1,29 @@
+package com.cyanice.shop.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class CustomerControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldGetCustomerWishlist() throws Exception {
+        ResultActions response = mockMvc.perform(get("/api/customer/1/wishlist")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)));
+    }
+}
